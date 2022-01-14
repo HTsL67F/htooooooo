fb00da704d73319afa11f14f153773dd27921482
sergeyzel <zelionniysergey@yandex.ru>
2022-01-14 11:45:31 +0700

    hto

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Chapter2.Task3;
+
+namespace ConsoleApp1
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Program.run(args);
+        }
+    }
+}
